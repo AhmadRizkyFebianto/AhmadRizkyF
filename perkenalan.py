9fcc771bb4372dd3ad6f9647d72d3d9b9c5bb8f8
AhmadRizkyFebianto <94149242+AhmadRizkyFebianto@users.noreply.github.com>
2021-11-12 14:18:24 +0700

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,13 @@
+nama = 'Ahmad Rizky Febianto'
+nim = 212410102097
+umur = '18 tahun' 
+tinggi_badan = 172.5
+berat_badan = 65 
+
+print(nama)
+print(nim)
+print(umur)
+print(tinggi_badan, 'cm')
+print(berat_badan, 'Kg')
+
+
